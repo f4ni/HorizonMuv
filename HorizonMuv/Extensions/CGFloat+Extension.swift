6a2df3a270d1b8b9bc88aefa765f8047ec8af860
furkan icoz <f_icoz@hotmail.com>
2022-10-16 23:08:10 +0300

    no message

---
+++ HorizonMuv/Extensions/CGFloat+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  CGFloat+Extension.swift
+//  HorizonMuv
+//
+//  Created by Furkan ic on 16.10.2022.
+//
+
+import UIKit
+
+extension CGFloat{
+    static let deviceWidth = UIScreen.main.bounds.width
+}
