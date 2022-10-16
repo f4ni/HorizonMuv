6f054924ef47bbcca1531a0d66b86d69dc3450de
furkan icoz <f_icoz@hotmail.com>
2022-10-16 23:12:26 +0300

    route for poster image added

--- HorizonMuv/Utilities/APIRouter.swift
+++ HorizonMuv/Utilities/APIRouter.swift
@@ -11,13 +11,19 @@ import Foundation
 enum APIRouter: URLRequestConvertible {
     
     case searchMovie(word: String)
+    case posterImage(url: String)
     
     static func movies(page: Int) -> String {
         "https://www.omdbapi.com/?apikey=\(page)&"
     }
     
     private var baseURL: String {
-        "https://www.omdbapi.com"
+        switch self {
+        case .searchMovie:
+            return "https://www.omdbapi.com"
+        case .posterImage(let url):
+            return url
+        }
     }
     
     private var apiKey: String {
@@ -27,7 +33,9 @@ enum APIRouter: URLRequestConvertible {
     private var path: String {
         switch self {
         case .searchMovie:
-            return "/?"
+            return "/?apikey=\(apiKey)"
+        case .posterImage:
+            return ""
         }
     }
     
@@ -35,28 +43,37 @@ enum APIRouter: URLRequestConvertible {
         switch self{
         case .searchMovie(let word):
             return [ "s" : word]
+        case .posterImage:
+            return nil
         }
     }
 
     func asURLRequest() throws -> URLRequest {
         
-        let url = baseURL + path + "apikey=\(apiKey)"
-        
-        var urlRequest = try URLRequest(url: url, method: .get)
-        
-        var urlComponents = URLComponents()
+        let url = baseURL + path
 
-        var parameters = [URLQueryItem]()
-        
-        if let parameter {
-            parameters = parameter.map({ URLQueryItem(name: $0.key, value: String(describing: $0.value)) })
+        var urlRequest = try URLRequest(url: url, method: .get)
+
+        switch self {
+            
+        case .searchMovie:
+            var urlComponents = URLComponents()
+
+            var parameters = [URLQueryItem]()
+            
+            if let parameter {
+                parameters = parameter.map({ URLQueryItem(name: $0.key, value: String(describing: $0.value)) })
+            }
+            parameters.append( URLQueryItem(name: "apikey", value: apiKey))
+                               
+            urlComponents.queryItems = parameters
+            
+            urlRequest.url = urlComponents.url(relativeTo: urlRequest.url)
+            
+            return urlRequest
+            
+        case .posterImage:
+            return urlRequest
         }
-        parameters.append( URLQueryItem(name: "apikey", value: apiKey))
-                           
-        urlComponents.queryItems = parameters
-        
-        urlRequest.url = urlComponents.url(relativeTo: urlRequest.url)
-        
-        return urlRequest
     }
 }
